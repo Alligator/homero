e64316f8e292d0e43364fbede2ada870f6015037
darvell <contact@darvell.me>
2011-09-15 02:20:59 +1000

    added .sponge

--- plugins/lilpp.py
+++ plugins/lilpp.py
@@ -1,7 +1,7 @@
 from util import hook
 from linecache import getline
 from random import randint
-
+from random import choice
 f = open("plugins/lilpp_list.txt", 'r')
 num_lines = len(f.readlines())
 f.close()
@@ -10,5 +10,14 @@ f.close()
 def lilpp(inp, say=None):
     say("< Lilpp> i hate " + getline("lilpp_list.txt", randint(0, num_lines-1)))
     return
-
+def sponge(inp, say=None):
+    spongeList = [
+        "crate",
+        "menu",
+        "shadow",
+        "robot man",
+        "zombie island"
+        ]
+    say("<sponge-> i hate " + choice(spongeList))
+    return
 
