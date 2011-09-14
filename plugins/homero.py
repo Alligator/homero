f7a50fe07bd05a28b377f5cd1b7ce90a63789401
darvell <contact@darvell.me>
2011-09-15 01:44:15 +1000

    made def for search url string, added sylauxe...not sure if its escaping spaces correctly!

--- plugins/homero.py
+++ plugins/homero.py
@@ -2,14 +2,25 @@
 import random
 from util import hook, http
 
-search_url = "https://gdata.youtube.com/feeds/api/videos?v=2&alt=jsonc&category=los%2Csimpsons&max-results=1&start-index="
+#search_url = "https://gdata.youtube.com/feeds/api/videos?v=2&alt=jsonc&category=los%2Csimpsons&max-results=1&start-index="
 yt_url = "http://youtu.be/%s/"
+
+def searchURL(cat):
+    return "https://gdata.youtube.com/feeds/api/videos?v=2&alt=jsonc&category=" + cat + "&max-results=1&start-index=" + random.randint(1,20)
 
 @hook.command(autohelp=False)
 def homero(inp):
     '.homero <query> -- los simpsons?'
-    start = random.randint(1, 20)
-    j = http.get_json(search_url + str(start))
+    j = http.get_json(searchURL("los simpsons"))
+    if j['data']['totalItems'] == 0:
+        return 'no results found'
+
+    title = j['data']['items'][0]['title']
+    vid = j['data']['items'][0]['id']
+    return title + ' - ' + yt_url % vid
+@hook.command
+def sylauxe(inp):
+    j = http.get_json(searchURL("diaper"))
 
     if j['data']['totalItems'] == 0:
         return 'no results found'
