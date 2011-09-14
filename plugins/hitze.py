93220c8d8fa42aaa33670e8687413ed5b63063ca
Darvell Long <contact@darvell.me>
2011-09-14 17:31:40 +1000

    hitze plugin (random reddit link grabber)
    todo: make the different subreddits into a list or something to make it look like REAL HARDCORE PROGRAMMING ACTION

---
+++ plugins/hitze.py
@@ -0,0 +1,18 @@
+import simplejson as json
+import random
+
+@hook.command
+def hitze(inp):
+    hitzelist = [
+      "ahahaaha",
+      "rofl, epic",
+      "omg.",
+      "uugh",
+      "why..",
+      "lol pcgaming",
+      "rip"
+    ]
+
+    jsonData = http.get_json('http://www.reddit.com/r/funny+pics+cars+cartalk+geek+xbox+gaming+minecraft+terraria/.json')
+
+    return "<@hitzler> " + random.choice(jsonData['data']['children'])['data']['url'] + " " + random.choice(hitzelist)
