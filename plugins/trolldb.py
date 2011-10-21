1331bf3657d2945d419519c900b91aa1073a6ec5
Alligator <enemy.forest.brigade@gmail.com>
2011-10-21 22:52:27 +0100

    fatgoon (its u)

--- plugins/trolldb.py
+++ plugins/trolldb.py
@@ -4,7 +4,14 @@ import re
 
 lines = [l for l in open('plugins/trolldb.txt', 'r').read().split('%') if len(l) < 500]
 
+goonlines = [l for l in open('plugins/fatgoon.txt', 'r').read().split('%') if len(l) < 500]
+
 @hook.command(autohelp=False)
 def truth(inp, say=None):
     say(re.sub('\n', '', choice(lines)))
+    return
+
+@hook.command(autohelp=False)
+def fatgoon(inp, say=None):
+    say(re.sub('\n', '', choice(goonlines)))
     return
