d0337dbab26d75fe7f3e1afb5e17b487f701e353
Darvell Long <contact@darvell.me>
2011-11-14 01:30:13 +1100

    added a test web interface thing, returns some ghetto json for AJAX WEB TWO POINT OH dayta analayaziz. json probably invalid lol listenin 2 breakcore and lookin at crossdressers while i write this this is a bad commit message

---
+++ plugins/webinterface.py
@@ -0,0 +1,47 @@
+# yeah it's bad but fuck you it works and you prob wont be able to do it any better (owned)
+# specify first argument of bot.py to be the port number u want
+
+import web
+import thread
+import json
+from util import hook, http
+
+webSql = web.database(dbn='sqlite',db='./persist/homero.irc.synirc.net.db')
+urls = (
+    '/ythistory', 'ythistory',
+    )
+app = web.application(urls, globals())
+
+class ythistory:
+    def GET(self):
+    	data = web.input(channel="",username="")
+    	goodass = dict()
+    	
+    	# ugly, u do it better tho bitch??
+    	query = ''
+    	if data.channel:
+    		query += ' chan = $channel '
+    	
+    	if data.username:
+    		if data.channel:
+    			query += ' AND user = $user'
+    		else:
+    			query += ' user = $user'
+
+    	result = webSql.select('ytmemory',order="time DESC",vars={'channel': '#' + data.channel,'user':data.username},where=query)
+    	
+    	#Json lib was stripping results and i literally do not care to investigate it, enjoy ghetto
+    	shitJson = "{\n\"videos\": [\n"
+    	for i in result:
+    		shitJson += "{\n\"id\": \"" + i.vid + "\",\n"
+    		shitJson += "\"title\": \"" + i.title + "\",\n"
+    		shitJson += "\"user\": \"" + i.user + "\",\n"
+    		shitJson += "\"channel\": \"" + i.chan + "\",\n"
+    		shitJson += "\"time\": \"" + str(i.time) + "\"\n},\n"
+    	shitJson += ']}'
+        return shitJson
+
+def runServer():
+	app.run()
+
+thread.start_new_thread(runServer, ())
