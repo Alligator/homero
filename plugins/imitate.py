0f010a83d733ff2dec9427d41cf22520cf1833cd
Reece Selwood <enemy.forest.brigade@gmail.com>
2011-12-05 00:42:42 +0000

    changed imitate to not use pairs not triplets

--- plugins/imitate.py
+++ plugins/imitate.py
@@ -3,36 +3,36 @@ from urllib2 import HTTPError
 import random
 import collections
 
-#@hook.command
+@hook.command
 def imitate(inp):
     ".imitate <account> -- use a markov text generator to imiatate a twitter account"
-    url = "http://api.twitter.com/1/statuses/user_timeline/%s.json?count=200" % inp
+    url = "http://api.twitter.com/1/statuses/user_timeline/%s.json?count=100" % inp
     try:
         tweets = [t['text'] for t in http.get_json(url)]
-    except HTTPError:
-        return "/!\\ either you or twitter is dumb but something is wrong and this didnt work sorry!!!! /!\\"
+    except HTTPError, e:
+        if e.code == 404:
+            return "user not found"
+        else:
+            return "something is wrong. i think im rate limited o god hhjelp gouys??"
 
     # learn that text
     # omg markov chains
     suffixes = collections.defaultdict(list)
     w1 = ""
-    w2 = ""
     for tweet in tweets:
         for word in tweet.split():
             if '@' in word: break
             if '#' in word: break
-            suffixes[(w1, w2)].append(word)
-            w1, w2 = w2, word
+            suffixes[w1].append(word)
+            w1 = word
         w1 = ""
-        w2 = ""
-    suffixes[(w1, w2)].append("")
+    suffixes[w1].append("")
 
     # now make a reply
     w1 = ""
-    w2 = ""
     out = ""
     for i in range(20):
-        sf = suffixes[(w1, w2)]
+        sf = suffixes[(w1)]
         try:
             next = random.choice(sf)
         except IndexError:
@@ -40,6 +40,6 @@ def imitate(inp):
         if next is "":
             break
         out += next + " "
-        w1, w2 = w2, next
+        w1 = next
 
     if out: return out
