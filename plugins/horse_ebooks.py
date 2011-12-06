3bc64368afa643fd56268261dee049dd1347ec5d
ally gator <alligator@rubix.alligate.org>
2011-12-06 21:16:52 +0000

    changing code in three places means: make it a function! also kill hitzler

--- plugins/horse_ebooks.py
+++ plugins/horse_ebooks.py
@@ -1,18 +1,19 @@
 from util import hook, http
 import time
 
-url = "http://api.twitter.com/1/statuses/user_timeline/Horse_ebooks.json?count=1"
+def get_tweet():
+    url = "http://api.twitter.com/1/statuses/user_timeline/Horse_ebooks.json?count=1"
+    return http.get_json(url)[0]['text'].replace('\n', ' ')
 
 last = time.time()
-last_tweet = http.get_json(url)[0]['text']
+last_tweet = get_tweet()
 
 @hook.event('*')
 def horse(inp, conn=None):
     global last, url, last_tweet
     if time.time() - last > 240:
         last = time.time()
-        j = http.get_json(url)
-        tweet = j[0]['text']
+        tweet = get_tweet()
         if tweet == last_tweet:
             return
         conn.cmd('PRIVMSG #sa-minecraft Horse_ebooks: %s' % tweet)
@@ -23,5 +24,5 @@ def horse_cmd(inp, say=None):
     global url, last, last_tweet
     if time.time() - last > 120:
         last = time.time()
-        last_tweet = http.get_json(url)[0]['text'].replace('\n', ' ')
+        last_tweet = get_tweet()
     say('Horse_ebooks: %s' % last_tweet)
