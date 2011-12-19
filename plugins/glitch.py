003677675ed45fa87cd652a461ee69f1753def2a
Reece Selwood <enemy.forest.brigade@gmail.com>
2011-12-19 22:47:00 +0000

    it works now, and supports not giving params. cool

--- plugins/glitch.py
+++ plugins/glitch.py
@@ -1,18 +1,23 @@
 from util import hook
-from poster.encode import multipart_encode
+from poster import encode
 from poster.streaminghttp import register_openers
+from itertools import izip_longest
 import urllib2
 
 @hook.command
 def glitch(inp):
     '.glitch url chance amount seed -- http://glitch.goonscape.org/'
-    url, chance, amount, seed = inp.split()
+    # ok im gonna forget what the heck i was doin here by tomorrow so lets write a proper comment
+    # split the params from inp but make them '' if they aren't there!!
+    p = izip_longest(('url', 'chance', 'amount', 'seed'), inp.split(), fillvalue='')
+    params = [a for a in p]
+
     register_openers()
-    datagen, headers = multipart_encode({'url': url,
-        'chance': chance,
-        'datalength': amount,
-        'randseend': seed})
+    boundary = encode.gen_boundary()
+
+    f = encode.MultipartParam('file', '', filename=' ', filetype='application/octet-stream')
+    params.insert(0, f)
+
+    datagen, headers = encode.multipart_encode(params, boundary)
     request = urllib2.Request('http://glitch.goonscape.org', datagen, headers)
-    conn = urllib2.urlopen(request)
-    print conn.read()
-    conn.close()
+    return urllib2.urlopen(request).geturl()
