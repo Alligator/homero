7aa64332cf35e8338f972bf18e828530a7052da9
ally gator <alligator@rubix.alligate.org>
2012-04-26 10:11:23 +0100

    hello, Mike.

---
+++ plugins/mike.py
@@ -0,0 +1,32 @@
+from util import hook
+from random import random, choice
+from time import time
+
+last = 0
+names = [
+    'Mike',
+    'Darv',
+    'Reece',
+    'Jess',
+    'Tuck',
+    'Matt',
+    'Nate',
+    'Shawn',
+    'Scott',
+    'Clay',
+]
+
+@hook.event('PRIVMSG')
+def mike(paraml, inp=None, say=None, chan=None):
+    global last
+
+    if last == 0:
+        last = time()
+
+    if 'http' in inp[1]:
+        return
+
+    if random() > 0.97 and chan == '#sa-minecraft' and time() - last > 600:
+        last = time()
+        say('%s, %s.' % (inp[1], choice(names)))
+
