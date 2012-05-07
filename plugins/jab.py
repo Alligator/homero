7b406c8b314a2900da4c174449553a101f8da862
ally gator <alligator@rubix.alligate.org>
2012-05-07 16:24:58 +0100

    added colour/format support to jab

--- plugins/jab.py
+++ plugins/jab.py
@@ -5,20 +5,29 @@ fill = u"\u25AC"
 
 @hook.command
 def jab(inp):
+    passchars = [2, 15, 18, 29, 31, 32]
     if len(inp) > 96:
         return
     out = ''
-    for c in inp:
-        if ord(c) <= 31:
+    bold = False
+    e = enumerate(inp)
+    for i, c in e:
+        if ord(c) == 3:
             out += c
+            out += inp[i+1]
+            if inp[i+2].isdigit():
+                out += inp[i+2]
+                e.next()
+            e.next()
+        elif ord(c) in passchars:
+            out += c
+        elif ord(c) <= 31:
+            out += c
+        elif ord(c) > 176:
             continue
-        if ord(c) == 32:
-            out += "  "
-            continue
-        if ord(c) > 176:
-            continue
-        uni = ord(c) + 65248
-        out += unichr(uni)
+        else:
+            uni = ord(c) + 65248
+            out += unichr(uni)
     return out
 
 @hook.command
