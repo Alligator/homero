531c9c973f2df7abbf63e6247e862062bafcbaf1
ally gator <alligator@rubix.alligate.org>
2012-08-05 13:27:14 +0100

    added time

---
+++ plugins/time.py
@@ -0,0 +1,6 @@
+from  util import hook, http
+
+@hook.command('time')
+def time_cmd(inp, reply=None):
+  t = http.get_json('http://www.worldweatheronline.com/feed/tz.ashx?key=a59771d9b2135559122607&q=%s&format=json' % inp)
+  reply(t['data']['request'][0]['query'] + ' | ' + t['data']['time_zone'][0]['localtime'])
