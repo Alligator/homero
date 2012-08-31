51a9fdb6fc9bdb12f5eaa2198888d40f45ec9c90
Reece Selwood <contact@alligatr.co.uk>
2012-08-31 10:36:01 +0100

    rewrote weather to use wunderground api. r.i.p google weather api

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,68 +1,43 @@
-"weather, thanks to google"
-
 from util import hook, http
-import lxml
 
-def get_weather_xml(nick, db, loc=None):
-  db.execute("create table if not exists weather(nick primary key, loc)")
-  
-  inp = loc
+@hook.command(autohelp=False)
+def weather(inp, nick=None, reply=None, db=None, bot=None):
+  'weather'
+  # init the db
+  db.execute('create table if not exists weather(nick primary key, loc)')
 
-  dontsave = loc.endswith(" dontsave")
-  if dontsave:
-    loc = loc[:-9].strip().lower()
+  api_url = 'http://api.wunderground.com/api/{0}/conditions/q/'.format(bot.config['api_keys']['wunderground'])
 
-  if not loc:
-    loc = db.execute("select loc from weather where nick=lower(?)",
-        (nick,)).fetchone()
-    if not loc:
+  # location and dontsave
+  dontsave = inp.endswith(' dontsave')
+  if inp and dontsave:
+    inp = inp[:-9].strip().lower()
+
+  # no location:
+  if not inp:
+    inp = db.execute('select loc from weather where nick=lower(?)', (nick,)).fetchone()
+    # no location in db
+    if not inp:
       return weather.__doc__
-    loc = loc[0]
-
-  # gd it google api
-  done = False
-  count = 0
-  while not done and count < 5:
-    try:
-      w = http.get_xml('http://www.google.com/ig/api', weather=loc)
-      done = True
-    except lxml.etree.XMLSyntaxError, e:
-      continue
-
-  if inp and not dontsave:
-    db.execute("insert or replace into weather(nick, loc) values (?,?)",
-        (nick.lower(), loc))
+    inp = inp[0]
+  # just location
+  elif not dontsave:
+    db.execute('insert or replace into weather(nick, loc) values (?,?)', (nick.lower(), inp))
     db.commit()
 
-  return w.find('weather')
+  response = http.get_json(api_url + inp + '.json')
+  try:
+    return response['response']['error']['description']
+  except KeyError, e:
+    pass
 
-@hook.command(autohelp=False)
-def weather(inp, nick='', reply=None, db=None):
-  ".weather <location> [dontsave] -- gets weather data from Google"
-  w = get_weather_xml(nick, db, inp)
+  # if we get multiple matches pick the first and re-request
+  try:
+    matches = response['response']['results']
+    response = http.get_json(api_url + matches[0]['zmw'] + '.json')
+  except KeyError, e:
+    pass
 
-  if w.find('problem_cause') is not None:
-    return "couldn't fetch weather data for '%s', try using a zip or " \
-        "postal code." % inp
+  name = response['current_observation']['display_location']['full']
 
-  info = dict((e.tag, e.get('data')) for e in w.find('current_conditions'))
-  info['city'] = w.find('forecast_information/city').get('data')
-  info['high'] = w.find('forecast_conditions/high').get('data')
-  info['low'] = w.find('forecast_conditions/low').get('data')
-
-  reply('%(city)s: %(condition)s, %(temp_f)sF/%(temp_c)sC (H:%(high)sF'\
-      ', L:%(low)sF), %(humidity)s, %(wind_condition)s.' % info)
-
-@hook.command(autohelp=False)
-def forecast(inp, nick=None, reply=None, db=None):
-  w = get_weather_xml(nick, db, inp)
-
-  if w.find('problem_cause') is not None:
-    return "couldn't fetch weather data for '%s', try using a zip or " \
-        "postal code." % inp
-
-  # im laffin v. hard at this monstrosity
-  out = ' | '.join(['{0}: H:{1}F L:{2}F. {3}'.format(a.find('day_of_week').get('data'), a.find('high').get('data'), a.find('low').get('data'), a.find('condition').get('data')) for a in w.findall('forecast_conditions')])
-  city = w.find('forecast_information/city').get('data')
-
-  reply('{0} | {1}'.format(city, out))
+  return '{name} - {weather}, {temperature_string}, feels like {feelslike_string}'.format(name=name, **response['current_observation'])
