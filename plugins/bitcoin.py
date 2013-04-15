bbc1a82a3c41a3f073d57b8c6c0a0a91be666a2c
Reece Selwood <contact@alligatr.co.uk>
2013-04-15 15:33:24 +0100

    new mtgox api

--- plugins/bitcoin.py
+++ plugins/bitcoin.py
@@ -1,10 +1,15 @@
 from util import http, hook
 
-
 @hook.command(autohelp=False)
 def bitcoin(inp, say=None):
     ".bitcoin -- gets current exchange rate for bitcoins from mtgox"
-    data = http.get_json("https://mtgox.com/code/data/ticker.php")
-    ticker = data['ticker']
-    say("Current: \x0307$%(buy).2f\x0f - High: \x0307$%(high).2f\x0f"
-        " - Low: \x0307$%(low).2f\x0f - Volume: %(vol)s" % ticker)
+    data = http.get_json("https://data.mtgox.com//api//2//BTCUSD//money//ticker")
+    ticker = data['data']
+    t = {
+      'low':  float(ticker['low']['value']),
+      'high': float(ticker['high']['value']),
+      'avg':  float(ticker['last']['value']),
+      'vol':  float(ticker['vol']['value'])
+    }
+    say("Current: \x0307$%(avg).2f\x0f - High: \x0307$%(high).2f\x0f"
+        " - Low: \x0307$%(low).2f\x0f - Volume: %(vol)s" % t)
