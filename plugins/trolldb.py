55c3a3e744bb59167794f650a5fbc0115a8666d2
Reece Selwood <contact@alligatr.co.uk>
2013-06-01 23:09:49 +0100

    jerk, and danl u got owned

--- plugins/trolldb.py
+++ plugins/trolldb.py
@@ -1,9 +1,13 @@
 from util import hook
 from random import choice
+import HTMLParser
 import re
 
 # we got some serious newline problems
 lines = [l for l in open('plugins/trolldb.txt', 'Ur').read().split('%') if len(l) < 500]
+
+jerklines = open('plugins/jerk.txt', 'Ur').readlines()
+h = HTMLParser.HTMLParser()
 
 goonlines = [l for l in open('plugins/fatgoon.txt', 'Ur').read().split('%') if len(l) < 500]
 
@@ -12,7 +16,12 @@ def truth(inp, say=None):
     say(re.sub('\n', '', choice(lines)))
     return
 
+@hook.command
+def jerk(inp, say=None):
+  say(h.unescape(choice(jerklines)))
+
 @hook.command(autohelp=False)
+@hook.command('danl')
 def fatgoon(inp, say=None):
     say(re.sub('\n', '', choice(goonlines)))
     return
