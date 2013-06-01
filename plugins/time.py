6cb47a739567a2d3caf9b38b28ba461013782e0c
Reece Selwood <contact@alligatr.co.uk>
2013-06-01 23:11:35 +0100

    lodon

--- plugins/time.py
+++ plugins/time.py
@@ -2,5 +2,7 @@ from  util import hook, http
 
 @hook.command('time')
 def time_cmd(inp, reply=None):
+  if inp == 'lodon':
+    inp = 'london'
   t = http.get_json('http://www.worldweatheronline.com/feed/tz.ashx?key=a59771d9b2135559122607&q=%s&format=json' % inp)
   reply(t['data']['request'][0]['query'] + ' | ' + t['data']['time_zone'][0]['localtime'])
