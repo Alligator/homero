86ab9ebfbe3a30a79ca07c0644c94e02943c715f
Reece Selwood <contact@alligatr.co.uk>
2013-06-01 23:06:08 +0100

    this is a terrible idea

--- plugins/bully.py
+++ plugins/bully.py
@@ -1,5 +1,23 @@
 from util import hook
+from time import sleep
+
+chans = {}
 
 @hook.command
 def bully(inp, say=None):
   say('{}: I feel offended by your recent action(s). Please read http://stop-irc-bullying.eu/stop'.format(inp))
+
+@hook.command(adminonly=True)
+def allbully(inp, chan=None, conn=None, say=None):
+  if chan.startswith('#'):
+    conn.cmd('NAMES {}'.format(chan))
+    sleep(2) # im the worst
+    for name in chans[chan]:
+      bully(name, say)
+
+@hook.event('353')
+def get_names(paraml):
+  global chans
+  chan = paraml[2]
+  names = paraml[3:]
+  chans[chan] = names[0].strip().split(' ')
