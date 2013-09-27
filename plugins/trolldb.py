9c0a91b7cacb500bb9ea5d105f4c1abd8783c2b3
Reece Selwood <contact@alligatr.co.uk>
2013-09-27 14:39:25 +0100

    Deep Thougts

--- plugins/trolldb.py
+++ plugins/trolldb.py
@@ -11,16 +11,22 @@ h = HTMLParser.HTMLParser()
 
 goonlines = [l for l in open('plugins/fatgoon.txt', 'Ur').read().split('%') if len(l) < 500]
 
+deeplines = [l.strip().split('\n') for l in open('plugins/deepthoughts.txt', 'Ur').read().split('%')]
+
 @hook.command(autohelp=False)
 def truth(inp, say=None):
     say(re.sub('\n', '', choice(lines)))
-    return
 
 @hook.command
 def jerk(inp, say=None):
-  say(h.unescape(choice(jerklines)))
+  say(h.unescape(choice(jerklines)).strip())
 
 @hook.command(autohelp=False)
 def fatgoon(inp, say=None):
     say(re.sub('\n', '', choice(goonlines)))
-    return
+
+@hook.command
+def deepthought(inp, say=None):
+  c = choice(deeplines)
+  say(c[0])
+  say(c[1])
