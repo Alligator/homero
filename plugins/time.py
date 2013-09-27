333c63b88cae37c72e20d8d755d92557479613ab
Reece Selwood <contact@alligatr.co.uk>
2013-09-27 14:41:35 +0100

    wwo depreciated their api, updated to use new one

--- plugins/time.py
+++ plugins/time.py
@@ -1,8 +1,12 @@
 from  util import hook, http
 
 @hook.command('time')
-def time_cmd(inp, reply=None):
+def time_cmd(inp, reply=None, bot=None):
   if inp == 'lodon':
     inp = 'london'
-  t = http.get_json('http://www.worldweatheronline.com/feed/tz.ashx?key=a59771d9b2135559122607&q=%s&format=json' % inp)
-  reply(t['data']['request'][0]['query'] + ' | ' + t['data']['time_zone'][0]['localtime'])
+  request = 'http://api.worldweatheronline.com/free/v1/tz.ashx?key={0}&q={1}&format=json'
+  j = http.get_json(request.format(bot.config['api_keys']['wwo'], http.quote_plus(inp)))
+  j = j['data']['time_zone'][0]
+  utc = j['utcOffset'].split('.')[0]
+  utc = '+' + utc if float(utc) >= 0 else utc
+  return '{0} (UTC {1})'.format(j['localtime'], utc)
