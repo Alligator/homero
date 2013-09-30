53562069ff43593a5119cfa28f7f32b66290db4e
Reece Selwood <contact@alligatr.co.uk>
2013-09-30 22:05:33 +0100

    added doctstring to units

--- plugins/units.py
+++ plugins/units.py
@@ -6,6 +6,7 @@ reg = re.compile(r'(.*) in (.*)')
 
 @hook.command
 def unit(inp):
+  ".unit <thing> in <thing> - converts unit, in is not optional"
   m = reg.findall(inp)[0]
   out = subprocess.check_output(['units', '-t',  m[0], m[1]]).strip()
   return '{} = {} {}'.format(m[0], out, m[1])
