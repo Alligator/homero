65eb04e28d0d1d0bf64d314b0aec8059b19b435a
Reece Selwood <contact@alligatr.co.uk>
2013-10-15 00:01:08 +0100

    not sure what to do with this...

--- plugins/webinterface.py
+++ plugins/webinterface.py
@@ -1,33 +1,33 @@
 # yeah it's bad but fuck you it works and you prob wont be able to do it any better (owned)
 # specify first argument of bot.py to be the port number u want
-
-import web
-import thread
-import json
-import pprint
-from util import hook, http
-
-webSql = web.database(dbn='sqlite',db='./persist/homero.irc.synirc.net.db')
-urls = (
-    '/ythistory', 'ythistory',
-    )
-app = web.application(urls, globals())
-
-class ythistory:
-    def GET(self):
-    	data = web.input(q="")
-
-        q = '%' + data.q + '%'
-
-        if data.q:
-            result = webSql.select('ytmemory', where='title like $q or user like $q or chan like $q order by time desc', vars={'q':q})
-        else:
-            result = webSql.select('ytmemory', order="time desc")
-
-        return json.dumps([i for i in result])
-    	
-
-def runServer():
-	app.run()
-
-thread.start_new_thread(runServer, ())
+# 
+# import web
+# import thread
+# import json
+# import pprint
+# from util import hook, http
+# 
+# webSql = web.database(dbn='sqlite',db='./persist/homero.irc.synirc.net.db')
+# urls = (
+#     '/ythistory', 'ythistory',
+#     )
+# app = web.application(urls, globals())
+# 
+# class ythistory:
+#     def GET(self):
+#     	data = web.input(q="")
+# 
+#         q = '%' + data.q + '%'
+# 
+#         if data.q:
+#             result = webSql.select('ytmemory', where='title like $q or user like $q or chan like $q order by time desc', vars={'q':q})
+#         else:
+#             result = webSql.select('ytmemory', order="time desc")
+# 
+#         return json.dumps([i for i in result])
+#     	
+# 
+# def runServer():
+# 	app.run()
+# 
+# thread.start_new_thread(runServer, ())
