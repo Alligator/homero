84c6a5491008816a5d3daf2364f5a003e5957c29
Reece Selwood <contact@alligatr.co.uk>
2013-10-16 21:17:49 +0100

    why did we do this again

--- plugins/trek.py
+++ plugins/trek.py
@@ -16,30 +16,3 @@ def trek(inp):
         r = treksum.generate_sentence(word_count=count, character=character)
         return r
 
-# oh my god
-@hook.command
-def picard(inp):    return trek('picard %s' % inp)
-@hook.command
-def riker(inp):     return trek('riker %s' % inp)
-@hook.command
-def data(inp):      return trek('data %s' % inp)
-@hook.command
-def worf(inp):      return trek('worf %s' % inp)
-@hook.command
-def geordi(inp):    return trek('geordi %s' % inp)
-@hook.command
-def barclay(inp):   return trek('barclay %s' % inp)
-@hook.command
-def q(inp):         return trek('q %s' % inp)
-@hook.command
-def pulaski(inp):   return trek('pulaski %s' % inp)
-@hook.command
-def computer(inp): return trek('computer %s' % inp)
-@hook.command
-def guinan(inp):    return trek('guinan %s' % inp)
-@hook.command
-def troi(inp):      return trek('troi %s' % inp)
-@hook.command
-def beverly(inp):   return trek('beverly %s' % inp)
-@hook.command
-def wesley(inp):    return trek('wesley %s' % inp)
