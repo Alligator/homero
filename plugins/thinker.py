7f71e3f50cfb7eeaad182cc225d8bb2f6a35408b
Reece Selwood <contact@alligatr.co.uk>
2013-10-16 21:17:36 +0100

    added multiline support to think

--- plugins/thinker.py
+++ plugins/thinker.py
@@ -1,5 +1,14 @@
 from util import hook
+import math
 
-@hook.command
+@hook.command(multiline=True)
 def think(inp, say=None):
-  return '(  .  __ . ) . o O ( {} )'.format(inp)
+  lines = inp.split('\n')
+  print repr(inp)
+  c = math.ceil(len(lines)/2.0)
+  out = ''
+  for i, line in enumerate(lines):
+    if i+1 == c:
+      say(u'(  .  __ . ) . o O ( {} )'.format(line))
+    else:
+      say(u'                     {}'.format(line))
