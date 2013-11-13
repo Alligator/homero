7d3340b3460992e5d9a72870a7ae56088dfaf8d8
rrs <contact@alligatr.co.uk>
2013-11-13 00:18:01 +0000

    rest in peace igoogle

--- plugins/convert.py
+++ plugins/convert.py
@@ -8,7 +8,5 @@ greg = re.compile(r"(?:l|r)hs: \"([^\"]*)")
 def convert(inp, say=None):
     m = reg.findall(inp)
     v1, c1, c2 = m[0]
-    j = http.get('http://www.google.com/ig/calculator?hl=en&q={0}{1}=?{2}'.format(v1, c1, c2))
-    g = greg.findall(j.decode('utf-8', errors='ignore'))
-    if j:
-      return '{0} = {1}'.format(*g)
+    h = http.get_html('https://www.google.co.uk/finance/converter?a={0}&from={1}&to={2}'.format(v1, c1, c2))
+    return h.xpath(r'//*[@id="currency_converter_result"]')[0].text_content()
