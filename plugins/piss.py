b4abdab982df389bdb57ed9f3a09d20e175f4257
rrs <contact@alligatr.co.uk>
2013-11-13 00:20:14 +0000

    horrible fixes

--- plugins/piss.py
+++ plugins/piss.py
@@ -5,16 +5,19 @@ from itertools import izip_longest
 
 @hook.command
 def piss(inp, say=None):
+  print repr(inp)
   dil = [
     "  |%\%-,_          \x0308",             
-    "/%\\\\.    '-,_       \x0308",             
+    "/%\\\\.    '-,_      \x0308",             
     "%|\%/        '-,_  \x0308",             
     "/%\\  --,__    ( \\`\x0308/",             
     "%(%  ;  %)%'-,_\__/\x0308",             
     "  /%   ,%/%        \x0308",             
     "    '%--'          "              
   ]
-  text = subprocess.check_output(shlex.split('figlet -f smscript -w 160 "' + inp.code('utf-8') + '"')).strip().split('\n')
-  text.insert(0, None)
+  # inp = inp.replace('"', '\\"').replace("'", "\\'")
+  text = subprocess.check_output(shlex.split('figlet -f smscript -w 160 "' + inp.encode('utf-8') + '"')).split('\n')
+  # text.insert(0, None)
   for dil, line in izip_longest(dil, text):
+    print line
     say(dil + (line if line else ''))
