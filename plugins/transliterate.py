cba3ad90544864a831b4522f751ec488e0a56a59
rrs <contact@alligatr.co.uk>
2014-02-04 15:24:20 +0000

    added .georgian and .bopomofo since apparently that exists

--- plugins/transliterate.py
+++ plugins/transliterate.py
@@ -33,14 +33,22 @@ def greekify(inp):
       (u'aw' , u'ω'),
       (u'Aw' , u'Ω'),
       (u'AW' , u'Ω'),
-      (u'(?:s|c)([^\w])', u'ς\g<1>'),
-      (u'ct' , u'кt'),
+      (u'ce' , u'σε'),
+      (u'CE' , u'ΣΕ'),
+      (u'Ce' , u'Σε'),
+      (u'ci' , u'σι'),
+      (u'CI' , u'ΣΙ'),
+      (u'Ci' , u'Σι'),
+      (u'cy' , u'συ'),
+      (u'CY' , u'ΣΥ'),
+      (u'Cy' , u'Συ'),
+      (u's([^\w])', u'ς\g<1>'),
     ],
     'single': [
       u'abcdefghijklmnopqrstuvwxyz'+
       u'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-      u'αβσδεφγηιικλμνοπκρστυβυξυζ'+
-      u'ΑΒΣΔΕΦΓΗΙΙΚΛΜΝΟΠΚΡΣΤΥΒΥΞΥΖ'
+      u'αβκδεφγηιικλμνοπκρστυβυξυζ'+
+      u'ΑΒΚΔΕΦΓΗΙΙΚΛΜΝΟΠΚΡΣΤΥΒΥΞΥΖ'
     ]
   }
   return transliterate(table, inp)
@@ -66,6 +74,59 @@ def russify(inp):
     'single': [
       u'abcdefghijklmnopqrstuvwxyz',
       u'абкдэфгхижклмноп рстувб ыз',
+    ]
+  }
+  return transliterate(table, inp.lower())
+
+@hook.command
+def georgify(inp):
+  table = {
+    'multi': [
+      (u"ts'", u'წ'),
+      (u"ch'", u'ჭ'),
+      (u"k'", u'ქ'),
+      (u"t'", u'თ'),
+      (u"p'", u'ფ'),
+      (u"sh", u'შ'),
+      (u"ch", u'ჩ'),
+      (u"zh", u'ჟ'),
+      (u"ts", u'ც'),
+      (u"dz", u'ძ'),
+      (u"kh", u'ხ'),
+      (u"ce", u'სე'),
+      (u"ci", u'სი'),
+      (u"cy", u'სი'),
+      (u"ck", u'კ'),
+      (u"x",  u"კს")
+    ],
+    'single': [
+      u'abcdefghijklmnopqrstuvwxyz',
+      u'აბკდეჶგჰიჯკლმნოპყრსტუვუ იზ'
+    ]
+  }
+  return transliterate(table, inp.lower())
+
+@hook.command
+def bopomofo(inp):
+  table = {
+    'multi': [
+      (u'ang', u'ㄤ'),
+      (u'eng', u'ㄥ'),
+      (u'ch',  u'ㄔ'),
+      (u'zh',  u'ㄓ'),
+      (u'sh',  u'ㄕ'),
+      (u'ai',  u'ㄞ'),
+      (u'ei',  u'ㄟ'),
+      (u'ao',  u'ㄠ'),
+      (u'ou',  u'ㄡ'),
+      (u'an',  u'ㄢ'),
+      (u'en',  u'ㄣ'),
+      (u'er',  u'ㄦ'),
+      (u'yu',  u'ㄩ')
+    ],
+    'single': [
+      u'abcdeêfghijklmnopqrstuüvwxyz',
+      u'ㄚㄅㄘㄉㄜㄝㄈㄍㄏㄧㄐㄎㄌㄇㄋㄛㄆㄑㄖㄙㄊㄨㄩㄩㄨㄒㄧㄗ'
     ]
   }
   return transliterate(table, inp.lower())
