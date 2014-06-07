aaf55841b34d71196e6e603be8b45fc10870bd97
rrs <contact@alligatr.co.uk>
2014-06-07 21:22:49 +0100

    animé

--- plugins/spike.py
+++ plugins/spike.py
@@ -4,3 +4,11 @@ import re
 @hook.regex('send(\s)+spike', re.IGNORECASE)
 def spike(inp, say=None):
   say('spike sent')
+
+@hook.regex('anime', re.IGNORECASE)
+def anime(inp):
+  return 'I think you mean animé.'
+
+@hook.regex('illwinter')
+def trillwinter(inp, say=None):
+  return say('TRILLWINTER')
