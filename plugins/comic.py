48020896f8e777e029b526f47b35dc29f491f504
rrs <contact@alligatr.co.uk>
2014-06-07 21:22:19 +0100

    comic title panels

--- plugins/comic.py
+++ plugins/comic.py
@@ -27,8 +27,8 @@ def comic_msg(paraml, input=None, bot=None):
 @hook.command("comic")
 def comic(paraml, input=None, db=None, bot=None, conn=None):
     #print os.getcwd()
-    if len(paraml) == 0:
-        paraml = input.chan
+    title = paraml
+    paraml = input.chan
     msgs = mcache[(paraml,conn)]
     sp = 0
     chars = set()
@@ -40,7 +40,6 @@ def comic(paraml, input=None, db=None, bot=None, conn=None):
         if sp > 10 or diff.total_seconds() > 120 or len(chars) > 5:
             break
 
-    #print sp, chars
     msgs = msgs[-1*sp:]
 
     panels = []
@@ -51,13 +50,15 @@ def comic(paraml, input=None, db=None, bot=None, conn=None):
             panels.append(panel)
             panel = []
         panel.append((char, msg))
-
     panels.append(panel)
 
     # fname = ''.join([random.choice("fartpoo42069") for i in range(16)]) + ".jpg"
     fname = datetime.now().isoformat().split('.')[0] + '-' + input.chan.replace('#', '') + '.jpg'
 
-    make_comic(chars, panels).save(os.path.join(savepath, fname), quality=85)
+    if len(title) > 0:
+      make_comic(chars, panels, title).save(os.path.join(savepath, fname), quality=86)
+    else:
+      make_comic(chars, panels).save(os.path.join(savepath, fname), quality=86)
     return "http://irc.alligatr.co.uk/homero/" + fname
 
 def wrap(st, font, draw, width):
@@ -95,7 +96,7 @@ def wrap(st, font, draw, width):
 def rendertext(st, font, draw, pos):
     ch = pos[1]
     for s in st:
-        s = s.encode('utf-8')
+        s = unicode(s)
         w, h = draw.textsize(s, font=font)
         for i in range(1,2):
           draw.text((pos[0]-i, ch-i), s, font=font, fill=(0x0,0x0,0x0,0x0))
@@ -103,6 +104,21 @@ def rendertext(st, font, draw, pos):
           draw.text((pos[0]+i, ch-i), s, font=font, fill=(0x0,0x0,0x0,0x0))
           draw.text((pos[0]+i, ch+i), s, font=font, fill=(0x0,0x0,0x0,0x0))
         draw.text((pos[0], ch), s, font=font, fill=(0xff,0xff,0xff,0xff))
+        ch += h
+
+def rendertitle(st, font, draw, pos):
+    w, h = draw.textsize(st[0], font=font)
+    ch = (300 - (h * len(st)))/2
+    for s in st:
+        s = unicode(s)
+        w, h = draw.textsize(s, font=font)
+        x = (450 - w)/2
+        for i in range(1,2):
+          draw.text((x-i, ch-i), s, font=font, fill=(0x0,0x0,0x0,0x0))
+          draw.text((x-i, ch+i), s, font=font, fill=(0x0,0x0,0x0,0x0))
+          draw.text((x+i, ch-i), s, font=font, fill=(0x0,0x0,0x0,0x0))
+          draw.text((x+i, ch+i), s, font=font, fill=(0x0,0x0,0x0,0x0))
+        draw.text((x, ch), s, font=font, fill=(0xff,0xff,0xff,0xff))
         ch += h
 
 def fitimg(img, (width, height)):
@@ -119,7 +135,7 @@ def fitimg(img, (width, height)):
 
     return img.resize((int(l[0]), int(l[1])), Image.ANTIALIAS)
 
-def make_comic(chars, panels):
+def make_comic(chars, panels, title=False):
     #filenames = os.listdir(os.path.join(os.getcwd(), 'chars'))
 
     panelheight = 300
@@ -132,16 +148,35 @@ def make_comic(chars, panels):
     chars = zip(chars, filenames)
     charmap = dict()
     for ch, f in chars:
-        charmap[ch] = Image.open(f)
-
+        if os.path.isdir(f):
+          charmap[ch] = []
+          for fi in os.listdir(f):
+            charmap[ch].append(Image.open(os.path.join(f, fi)))
+        else:
+          charmap[ch] = [Image.open(f)]
 
     imgwidth = panelwidth
     imgheight = panelheight * len(panels)
+    if title:
+      imgheight += panelheight
 
     bg = Image.open(os.path.join('comic/backgrounds', random.choice(os.listdir('comic/backgrounds'))))
 
     im = Image.new("RGBA", (imgwidth, imgheight), (0xff, 0xff, 0xff, 0xff))
     font = ImageFont.truetype("comic/COMICBD.TTF", 14)
+    titlefont = ImageFont.truetype("comic/COMICBD.TTF", 20)
+
+    if title:
+        pim = Image.new("RGBA", (panelwidth, panelheight), (0xff, 0xff, 0xff, 0xff))
+        pim.paste(bg, (0, 0))
+        draw = ImageDraw.Draw(pim)
+        st1w = 0; st1h = 0; st2w = 0; st2h = 0
+        (st1, (st1w, st1h)) = wrap(title, titlefont, draw, 2*panelwidth/3.0)
+        rendertitle(st1, titlefont, draw, (10, 10))
+
+        draw.line([(0, 0), (0, panelheight-1), (panelwidth-1, panelheight-1), (panelwidth-1, 0), (0, 0)], (0, 0, 0, 0xff))
+        del draw
+        im.paste(pim, (0, 0))
 
     for i in xrange(len(panels)):
         pim = Image.new("RGBA", (panelwidth, panelheight), (0xff, 0xff, 0xff, 0xff))
@@ -160,19 +195,22 @@ def make_comic(chars, panels):
             texth += st2h + 10 + 5
 
         maxch = panelheight - texth
-        im1 = fitimg(charmap[panels[i][0][0]], (2*panelwidth/5.0-10, maxch))
+        im1 = fitimg(random.choice(charmap[panels[i][0][0]]), (2*panelwidth/5.0-10, maxch))
         try:
           pim.paste(im1, (10, panelheight-im1.size[1]), im1)
         except Exception, e:
           print e, st2
 
         if len(panels[i]) == 2:
-            im2 = fitimg(charmap[panels[i][1][0]], (2*panelwidth/5.0-10, maxch))
+            im2 = fitimg(random.choice(charmap[panels[i][1][0]]), (2*panelwidth/5.0-10, maxch))
             im2 = im2.transpose(Image.FLIP_LEFT_RIGHT)
             pim.paste(im2, (panelwidth-im2.size[0]-10, panelheight-im2.size[1]), im2)
 
         draw.line([(0, 0), (0, panelheight-1), (panelwidth-1, panelheight-1), (panelwidth-1, 0), (0, 0)], (0, 0, 0, 0xff))
         del draw
-        im.paste(pim, (0, panelheight * i))
+        if title:
+          im.paste(pim, (0, panelheight * (i+1)))
+        else:
+          im.paste(pim, (0, panelheight * i))
 
     return im
