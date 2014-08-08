2bb78da02ca4781739cc6ea32c67fc8acc00ad46
rrs <contact@alligatr.co.uk>
2014-08-08 19:56:36 +0100

    This Woman Abandoned Her Weather API. Here's Why You Should Too.

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,53 +1,55 @@
-from util import hook, http
-from pprint import pprint
+from util import hook
+import json
+import requests
 
 @hook.command(autohelp=False)
 def weather(inp, bot=None, reply=None, nick=None, db=None):
-  '.weather <location> [dontsave] -- gets weather data for location'
-
-  request = 'http://api.worldweatheronline.com/free/v1/weather.ashx?key={0}&q={1}&num_of_days=1&format=json'
-
-  loc = inp
-
-  # init db
-  db.execute("create table if not exists weather(nick primary key, loc)")
-
+  db.execute("create table if not exists weather(nick primary key, loc, lat, lng, desc)")
   dontsave = inp.endswith(' dontsave')
   if dontsave:
     loc = inp[:-9]
-
   loc = inp.lower().strip()
 
-  # no address given
+  addr = loc
+  lat = None
+  lng = None
+  desc = None
   if not loc:
-    loc = db.execute("select loc from weather where nick=lower(?)",
+    loc = db.execute("select loc, lat, lng, desc from weather where nick=lower(?)",
                      (nick,)).fetchone()
     if not loc:
       return weather.__doc__
-    loc = loc[0]
+    addr, lat, lng, desc = loc
 
-  try:
-    j = http.get_json(request.format(bot.config['api_keys']['wwo'], http.quote_plus(loc)))
-  except IOError, e:
-    print e
-    return 'um the api broke or something'
+  if not lat or not lng or not desc:
+    # do geocoding req
+    url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    params = { 'address': addr, 'key': bot.config['api_keys']['geocoding'] }
+    data = json.loads(requests.get(url, params=params).text)
+    geo = data['results'][0]['geometry']['location']
+    geo['desc'] = data['results'][0]['formatted_address']
+  else:
+    geo = { 'lat': lat, 'lng': lng, 'desc': desc }
 
-  if 'current_condition' not in j['data']:
-    return 'Location not found'
-  
-  w = j['data']['current_condition'][0]
-  w['desc'] = w['weatherDesc'][0]['value']
-  w['city'] = j['data']['request'][0]['query']
+  url = 'https://api.forecast.io/forecast/{}/{},{}'.format(bot.config['api_keys']['forecast'], geo['lat'], geo['lng'])
+  data = json.loads(requests.get(url, params={'units': 'si'}).text)
 
-  t = j['data']['request'][0]['type']
-  if t == 'Zipcode':
-    j = http.get_json('http://api.zippopotam.us/us/' + w['city'])['places'][0]
-    w['city'] = '{place name}, {state}'.format(**j)
+  current = data['currently']
+  forecast = data['daily']['summary']
 
-  reply('{city}: {desc}, {temp_F}F/{temp_C}C, Humidity: {humidity}%, '\
-         'Wind: {windspeedKmph}kph/{windspeedMiles}mph'.format(**w))
+  summary = u'{} | {}C/{}F | Humidity: {} | Wind: {}kph/{}mph'.format(
+      current['summary'],     # summary
+      int(current['temperature']), # temp in c
+      int(current['temperature'] * (9.0/5.0)) + 32, # temp in f lmao
+      '{}%'.format(int(current['humidity'] * 100)),
+      int(round((current['windSpeed']*60*60)/1000)),
+      int(round((current['windSpeed']*60*60)/1000 * 0.621371)))
+  output = u'{} | {}'.format(geo['desc'], summary)
+  reply(output)
+  reply(forecast)
 
   if inp and not dontsave:
-    db.execute("insert or replace into weather(nick, loc) values (?,?)",
-               (nick.lower(), loc))
+    db.execute("insert or replace into weather(nick, loc, lat, lng, desc) values (?,?,?,?,?)",
+               (nick.lower(), addr, geo['lat'], geo['lng'], geo['desc']))
     db.commit()
+  return
