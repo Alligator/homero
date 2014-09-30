2b86b1a15e2e8e56c55adb331b8eee243ca5b50b
rrs <contact@alligatr.co.uk>
2014-09-30 15:31:01 +0100

    fix the name of units

--- plugins/units.py
+++ plugins/units.py
@@ -5,8 +5,8 @@ import subprocess
 reg = re.compile(r'(.*) in (.*)')
 
 @hook.command
-def unit(inp):
-  ".unit <thing> in <thing> - converts unit, in is not optional"
+def units(inp):
+  ".units <thing> in <thing> - converts unit, in is not optional"
   m = reg.findall(inp)[0]
   out = subprocess.check_output(['units', '-t',  m[0], m[1]]).strip()
   return '{} = {} {}'.format(m[0], out, m[1])
