83ea01d8729c6f0054eab3207a076e19ccc7b730
rrs <contact@alligatr.co.uk>
2015-04-27 21:43:12 +0100

    .tellgroup. the plugin no-one uses

--- plugins/tell.py
+++ plugins/tell.py
@@ -103,3 +103,94 @@ def tell(inp, nick='', chan='', db=None):
         return "Message has already been queued."
 
     return "I'll pass that along."
+
+@hook.command
+def tellgroup(inp, nick=None, chan=None, db=None, reply=None):
+  inp = inp.split(' ', 1)
+  if len(inp) != 2:
+    return 'no mate'
+  inp, msg = inp
+
+  try:
+    users = db.execute('select users from tellgroup where name=?',
+      (inp,)).fetchone()
+  except db.OperationalError:
+    return 'no groups are defined'
+
+  if not users:
+    return 'group not found'
+
+  users = users[0].split(' ')
+  sent = []
+  for user in users:
+    if user == nick: continue
+    ret = tell(user + ' ' + msg, nick=nick, chan=chan, db=db)
+    if ret != "I'll pass that along.":
+      reply('error telling ' + user + ': ' + ret)
+    else:
+      sent.append(user)
+
+@hook.command
+def joingroup(inp, nick=None, chan=None, db=None):
+  db.execute("create table if not exists tellgroup"
+              "(name, chan, users, primary key(name, chan))")
+  db.commit()
+
+  inp = inp.split(' ')[0]
+
+  users = db.execute('select users from tellgroup where name=? and chan=?',
+              (inp, chan)).fetchone()
+
+  users = set(users[0].split(' ')) if users else set()
+  if nick in users:
+    return 'you are already in that group'
+  users.add(nick)
+
+  db.execute('insert or replace into tellgroup(name, chan, users) values(?,?,?)',
+              (inp, chan, ' '.join(users)))
+  db.commit()
+
+  return 'joined ' + inp
+
+@hook.command
+def leavegroup(inp, nick=None, chan=None, db=None):
+  try:
+    inp = inp.split(' ')[0]
+    users = db.execute('select users from tellgroup where name=? and chan=?',
+      (inp, chan)).fetchone()
+  except db.operationalerror:
+    return 'no groups are defined'
+
+  if users:
+    users = set(users[0].split(' '))
+
+    if nick not in users:
+      return 'you are not in that group'
+
+    users.remove(nick)
+
+    if len(users) == 0:
+      db.execute('delete from tellgroup where name=? and chan=?', (inp, chan))
+      db.commit()
+      return 'removed from ' + inp + ' and deleted the group as it is empty'
+    else:
+      db.execute('insert or replace into tellgroup(name, chan, users) values(?,?,?)',
+          (inp, chan, ' '.join(users)))
+      db.commit()
+      return 'removed from ' + inp
+  else:
+    return 'group not found'
+
+@hook.command
+def groups(inp, nick=None, chan=None, db=None):
+  try:
+    groups = db.execute('select name from tellgroup where users like ?',
+        ('%' + nick + '%',)).fetchall()
+  except db.OperationalError, e:
+    return 'no groups are defined'
+
+  if groups:
+    groups = zip(*groups)[0]
+    return 'for the channel {} you are in: {}'.format(chan, ', '.join(groups))
+  else:
+    return 'you are in no groups for {}'.format(chan)
