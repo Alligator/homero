a17a35b0a73ea46e7c8460b46421b95d4297eba2
rrs <contact@alligatr.co.uk>
2015-04-27 21:43:26 +0100

    units path

--- plugins/units.py
+++ plugins/units.py
@@ -8,5 +8,5 @@ reg = re.compile(r'(.*) in (.*)')
 def units(inp):
   ".units <thing> in <thing> - converts unit, in is not optional"
   m = reg.findall(inp)[0]
-  out = subprocess.check_output(['units', '-t',  m[0], m[1]]).strip()
+  out = subprocess.check_output(['/usr/bin/units', '-t',  m[0], m[1]]).strip()
   return '{} = {} {}'.format(m[0], out, m[1])
