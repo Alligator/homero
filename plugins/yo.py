e46e41849504f05eb97f1643504b0bb5b6c01f4a
rrs <contact@alligatr.co.uk>
2015-04-27 21:43:33 +0100

    see ya yo

--- plugins/yo.py
+++ plugins/yo.py
@@ -5,7 +5,7 @@ import re
 import urlparse
 import json
 
-@hook.command(limit=1, autohelp=False)
+# @hook.command(limit=1, autohelp=False)
 def yo(inp, chan=None, bot=None):
   ".yo [username] [link] -- annoy everyone. (send a yo to HOMERYO to subscribe)."
   # if chan != '#sa-minecraft':
@@ -35,32 +35,30 @@ def yo(inp, chan=None, bot=None):
 
 latlngcache = {}
 
-@hook.event('*', limit=1)
+# @hook.event('*', limit=1)
 def checkyo(paraml, conn=None, bot=None):
   global latlngcache
   log = open('/tmp/yo.log', 'r').readlines()
   msg = ''
   for line in log:
-    m = re.search(r'name=(.*) ', line)
+    m = re.search(r'\?(.*) ', line)
     if m:
-      name = m.group(1)
-      if '&' in name:
-        name, query = name.split('&')
-        query = urlparse.parse_qs(query)
-        if 'location' in query:
-          if query['location'][0] in latlngcache:
-            addr = latlngcache[query['location'][0]]
-          else:
-            url = 'https://maps.googleapis.com/maps/api/geocode/json'
-            params = { 'latlng': query['location'][0].replace(';', ','), 'key': bot.config['api_keys']['geocoding'] }
-            data = json.loads(requests.get(url, params=params).text)
-            addr = data['results'][0]['formatted_address']
-            latlngcache[query['location'][0]] = addr
-          link = 'http://maps.google.com/maps?z=12&t=m&q=loc:{}'.format(query['location'][0].replace(';', '+'))
-          msg = '{} @ {} {}'.format(name, addr, link)
-        if 'link' in query:
-          msg = '{} {}'.format(name, query['link'][0])
-      else:
-        msg = name
+      qs = m.group(1)
+      query = { k: v[0] if v[0] != 'None' else None for k, v in urlparse.parse_qs(qs).iteritems() }
+      name = query['username']
+      if query['location']:
+        if query['location'] in latlngcache:
+          addr = latlngcache[query['location']]
+        else:
+          url = 'https://maps.googleapis.com/maps/api/geocode/json'
+          params = { 'latlng': query['location'].replace(';', ','), 'key': bot.config['api_keys']['geocoding'] }
+          data = json.loads(requests.get(url, params=params).text)
+          print params
+          addr = data['results'][0]['formatted_address']
+          latlngcache[query['location']] = addr
+        link = 'http://maps.google.com/maps?z=12&t=m&q=loc:{}'.format(query['location'].replace(';', '+'))
+        msg = '{} @ {} {}'.format(name, addr, link)
+      if query['link']:
+        msg = '{} {}'.format(name, query['link'])
       conn.cmd('privmsg #sa-minecraft Yo from ' + msg)
   open('/tmp/yo.log', 'w').close() # clear the file
