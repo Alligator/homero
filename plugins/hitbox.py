67e98dc96f7361b68d1b97f5ad438a420844b60a
rrs <contact@alligatr.co.uk>
2015-04-27 21:40:56 +0100

    hooray for undocumented api features

--- plugins/hitbox.py
+++ plugins/hitbox.py
@@ -4,16 +4,14 @@ streaming = {}
 
 @hook.event('*', limit=2)
 def hitbox(inp, conn=None):
+  pass
   global streaming
-  j = http.get_json('https://api.hitbox.tv/team/breadcrew?liveonly=true&media=true&fast=true')
+  j = http.get_json('https://api.hitbox.tv/team/breadcrew?media=true&media_type=live&liveonly=true')
   streams = j['media']['livestream']
-  if len(streams) == 0:
-    streaming = {}
-    return
   new_streaming = {}
   for stream in streams:
     name = stream['media_display_name']
-    if name not in streaming:
+    if stream['media_is_live'] == '1' and name not in streaming:
       conn.cmd('privmsg #sa-minecraft \x0313{} started streaming http://stream.alligatr.co.uk/'.format(name))
     new_streaming[name] = True
   streaming = new_streaming
