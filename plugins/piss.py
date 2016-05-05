6da73631a9c50a26ae21019cd2408202bca94888
rrs <contact@alligatr.co.uk>
2016-05-05 09:09:37 +0100

    .piss is usually the first command to fail when homero is running out of memory, so kill the bot if we get an out of memory error out of memory error

--- plugins/piss.py
+++ plugins/piss.py
@@ -2,10 +2,10 @@ from util import hook
 import shlex
 import subprocess
 from itertools import izip_longest
+import sys
 
 @hook.command
 def piss(inp, say=None):
-  print repr(inp)
   dil = [
     "  |%\%-,_          \x0308",             
     "/%\\\\.    '-,_      \x0308",             
@@ -16,8 +16,13 @@ def piss(inp, say=None):
     "    '%--'          "              
   ]
   # inp = inp.replace('"', '\\"').replace("'", "\\'")
-  text = subprocess.check_output(shlex.split('/usr/bin/figlet -f smscript -w 160 "' + inp.encode('utf-8') + '"')).split('\n')
-  # text.insert(0, None)
-  for dil, line in izip_longest(dil, text):
-    print line
-    say(dil + (line if line else ''))
+  try:
+    text = subprocess.check_output(shlex.split('/usr/bin/figlet -f smscript -w 160 "' + inp.encode('utf-8') + '"')).split('\n')
+    # text.insert(0, None)
+    for dil, line in izip_longest(dil, text):
+      print line
+      say(dil + (line if line else ''))
+  except OSError, e:
+    if e.errno == 12:
+      # outta memory, bail
+      sys.exit(0)
