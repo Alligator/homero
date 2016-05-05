1e7fdae6fc763195024c70d432398021708b2037
rrs <contact@alligatr.co.uk>
2016-05-05 09:12:50 +0100

    handle negative numbers when converting to fahrenheit

--- plugins/weather.py
+++ plugins/weather.py
@@ -40,7 +40,7 @@ def weather(inp, bot=None, reply=None, nick=None, db=None):
   current = data['currently']
   forecast = data['daily']['summary']
 
-  m = re.search(u'(\d+)°C', forecast)
+  m = re.search(u'([-\d]+)°C', forecast)
   if m:
     t = str(int(int(m.group(1)) * (9.0/5.0)) + 32)
     e = m.end(0)
