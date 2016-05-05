0ae52520bee31074d59745561596ead47378c795
rrs <contact@alligatr.co.uk>
2016-05-05 09:11:54 +0100

    sonic 2016

--- plugins/twitter_status.py
+++ plugins/twitter_status.py
@@ -1,7 +1,8 @@
 from util import hook, http
 from twython import Twython
+from random import choice
 
-twitter_re_status = r"(?i)twitter\.com/(?:#!/)?(\S+)/status/(\S+)"
+twitter_re_status = r"(?i)twitter\.com/(?:#!/)?(?P<user>\S+)/status/(\S+)"
 token = None
 
 def init(key, secret):
@@ -12,11 +13,18 @@ def init(key, secret):
 @hook.regex(twitter_re_status)
 def twitter_status(match, bot=None):
   global token
-  id = match.group(2)
+  result = {}
 
-  init(bot.config['api_keys']['twitter_key'], bot.config['api_keys']['twitter_secret'])
-  twitter = Twython(bot.config['api_keys']['twitter_key'], access_token=token)
-  result = twitter.show_status(id=id)
+  if match.group('user').lower() == 'realdonaldtrump':
+    init(bot.config['api_keys']['twitter_key'], bot.config['api_keys']['twitter_secret'])
+    twitter = Twython(bot.config['api_keys']['twitter_key'], access_token=token)
+    result = choice(twitter.get_user_timeline(screen_name="BadSonicFanArt"))
+  else:
+    id = match.group(2)
+
+    init(bot.config['api_keys']['twitter_key'], bot.config['api_keys']['twitter_secret'])
+    twitter = Twython(bot.config['api_keys']['twitter_key'], access_token=token)
+    result = twitter.show_status(id=id)
 
   at_name = result['user']['screen_name']
   full_name = result['user']['name']
