c588ddf3c9984598b77a1ac2aa1dcafe09e4f743
rrs <contact@alligatr.co.uk>
2017-02-07 01:19:04 +0000

    dom4 - added loadEvents=1 to all the URLs

--- plugins/dom4.py
+++ plugins/dom4.py
@@ -2,15 +2,15 @@ from util import hook
 from urllib import quote
 
 urls = {
-    'item':  'http://larzm42.github.io/dom4inspector/?page=item&itemq={}&showmoddinginfo=1&showids=1',
-    'hat':   'http://larzm42.github.io/dom4inspector/?page=item&itemq={}&showmoddinginfo=1&showids=1',
-    'spell': 'http://larzm42.github.io/dom4inspector/?page=spell&spellq={}&showmoddinginfo=1&showids=1',
-    'unit':  'http://larzm42.github.io/dom4inspector/?page=unit&unitq={}&showmoddinginfo=1&showids=1',
-    'site':  'http://larzm42.github.io/dom4inspector/?page=site&siteq={}&showmoddinginfo=1&showids=1',
-    'weapon':'http://larzm42.github.io/dom4inspector/?page=wpn&wpnq={}&showmoddinginfo=1&showids=1',
-    'armor': 'http://larzm42.github.io/dom4inspector/?page=armor&armorq={}&showmoddinginfo=1&showids=1',
-    'merc':  'http://larzm42.github.io/dom4inspector/?page=merc&mercq={}&showmoddinginfo=1&showids=1',
-    'event':  'http://larzm42.github.io/dom4inspector/?page=event&eventq={}&showmoddinginfo=1&showids=1'
+    'item':  'http://larzm42.github.io/dom4inspector/?page=item&itemq={}&showmoddinginfo=1&showids=1&loadEvents=1',
+    'hat':   'http://larzm42.github.io/dom4inspector/?page=item&itemq={}&showmoddinginfo=1&showids=1&loadEvents=1',
+    'spell': 'http://larzm42.github.io/dom4inspector/?page=spell&spellq={}&showmoddinginfo=1&showids=1&loadEvents=1',
+    'unit':  'http://larzm42.github.io/dom4inspector/?page=unit&unitq={}&showmoddinginfo=1&showids=1&loadEvents=1',
+    'site':  'http://larzm42.github.io/dom4inspector/?page=site&siteq={}&showmoddinginfo=1&showids=1&loadEvents=1',
+    'weapon':'http://larzm42.github.io/dom4inspector/?page=wpn&wpnq={}&showmoddinginfo=1&showids=1&loadEvents=1',
+    'armor': 'http://larzm42.github.io/dom4inspector/?page=armor&armorq={}&showmoddinginfo=1&showids=1&loadEvents=1',
+    'merc':  'http://larzm42.github.io/dom4inspector/?page=merc&mercq={}&showmoddinginfo=1&showids=1&loadEvents=1',
+    'event':  'http://larzm42.github.io/dom4inspector/?page=event&eventq={}&showmoddinginfo=1&showids=1&loadEvents=1'
 }
 
 @hook.command
